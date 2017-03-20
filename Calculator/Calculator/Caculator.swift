75c239f0c44bbc1198a88300a8fca756c3743e24
Chun Cao <chun@Chuns-iMac.local>
2017-03-20 09:33:00 +0800

    calculator

---
+++ Calculator/Calculator/Caculator.swift
@@ -0,0 +1,77 @@
+//
+//  Caculator.swift
+//  Calculator
+//
+//  Created by Chun Cao on 2016/9/26.
+//  Copyright © 2016年 NJU. All rights reserved.
+//
+
+import Foundation
+
+class Calculator: NSObject {
+    
+    enum Operation {
+        case UnaryOp((Double) ->Double)
+        case BinaryOp((Double, Double) -> Double)
+        case EqualsOp
+        case Constant(Double)
+    }
+    
+    var operations  = [
+        
+        "+": Operation.BinaryOp({(op1, op2) in
+            return op1+op2
+        }),
+        
+        "−": Operation.BinaryOp({(op1: Double, op2: Double) -> Double in
+            return op1-op2
+        }),
+        
+        "×": Operation.BinaryOp({ $0 * $1 }),
+        
+        "÷": Operation.BinaryOp({ $0 / $1 }),
+        
+        "=": Operation.EqualsOp,
+        
+        "C": Operation.UnaryOp({_ in return 0}),
+        
+        "±": Operation.UnaryOp({ -$0}),
+        
+        "√": Operation.UnaryOp({sqrt($0)}),
+        
+        "π": Operation.Constant(M_PI)
+        
+    ]
+    
+    
+    func performOperation(operation: String, operand: Double)  -> Double? {
+        
+        if let op = operations[operation]{
+            switch  op {
+            case .BinaryOp(let function):
+                pendingOp = Intermediate(firstOp: operand, waitingOperation: function)
+                return nil
+            case .UnaryOp(let function):
+                return function(operand)
+            case .EqualsOp:
+                if let theOperation =  pendingOp{
+                     return theOperation.waitingOperation(theOperation.firstOp, operand)
+                }
+            case .Constant(let value):
+                return value
+            }
+        }
+        return nil
+    }
+    
+    var pendingOp: Intermediate? = nil
+    
+    
+    struct Intermediate{
+        var firstOp: Double
+        var waitingOperation : (Double, Double) ->Double
+    }
+    
+    
+    
+}
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Chun Cao on 2016/9/26.
+//  Copyright © 2016年 NJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var calculator = Calculator()
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var inTypingMiddle = false
+    
+    @IBAction func numberTouched(_ sender: UIButton) {
+        
+        if let currentDigit = sender.currentTitle {
+            if inTypingMiddle{
+                digitOnDisplay = digitOnDisplay + currentDigit
+            }
+            else{
+                digitOnDisplay = currentDigit
+                inTypingMiddle = true
+            }
+        }
+    }
+    
+    var digitOnDisplay: String{
+        get{
+            return self.display.text!
+        }
+        set{
+            self.display.text = newValue
+        }
+    }
+    
+    
+    @IBAction func operatorTouched(_ sender: UIButton) {
+        
+        if let op = sender.currentTitle {
+            
+            
+            if let result = calculator.performOperation(operation: op, operand: Double(digitOnDisplay)!){
+                digitOnDisplay = String(result)
+            }
+            
+            inTypingMiddle = false
+        }
+        
+        
+    }
+}
+
